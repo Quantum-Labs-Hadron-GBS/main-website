0d70f0bb169b26108ea7d02821af53b7ce502d4f
Jayanth Ramakrishnan <jayanthvasudeva@gmail.com>
2026-07-20 14:00:37 +0530

    feat: replace text with partner logos in marquee

--- src/app/components/PartnerMarquee/PartnerMarquee.tsx
+++ src/app/components/PartnerMarquee/PartnerMarquee.tsx
@@ -1,7 +1,13 @@
 import styles from "./PartnerMarquee.module.css";
 
+import Image from "next/image";
+
 const PARTNERS = [
-  "ServiceNow", "Salesforce", "BMC", "Ivanti", "Atlassian", "SAP", "UiPath", "Ansible"
+  { name: "ServiceNow", logo: "https://res.cloudinary.com/djxbxhgat/image/upload/v1784523675/ServiceNow-Logo_kbaxks.png" },
+  { name: "BMC Software", logo: "https://res.cloudinary.com/djxbxhgat/image/upload/v1784523792/Logo_BMC_Software_oqiorh.png" },
+  { name: "Salesforce", logo: "https://res.cloudinary.com/djxbxhgat/image/upload/v1784523790/Salesforce-Logo_y5q2ir.png" },
+  { name: "Freshworks", logo: "https://res.cloudinary.com/djxbxhgat/image/upload/v1784523791/freshworks-logo_brandlogos.net_c6t5u_hu6w9j.png" },
+  { name: "Ivanti", logo: "https://res.cloudinary.com/djxbxhgat/image/upload/v1784523791/Logo_Ivanti_ww1j1c.png" }
 ];
 
 export default function PartnerMarquee() {
@@ -9,8 +15,16 @@ export default function PartnerMarquee() {
     <div className={styles.marqueeWrapper}>
       <div className={styles.marquee}>
         <div className={styles.marqueeContent}>
-          {PARTNERS.map((p, i) => <span key={i} className={styles.partner}>{p}</span>)}
-          {PARTNERS.map((p, i) => <span key={`dup-${i}`} className={styles.partner}>{p}</span>)}
+          {PARTNERS.map((p, i) => (
+            <div key={i} className={styles.partner}>
+              <Image src={p.logo} alt={p.name} width={120} height={40} className={styles.partnerLogo} />
+            </div>
+          ))}
+          {PARTNERS.map((p, i) => (
+            <div key={`dup-${i}`} className={styles.partner}>
+              <Image src={p.logo} alt={p.name} width={120} height={40} className={styles.partnerLogo} />
+            </div>
+          ))}
         </div>
       </div>
     </div>
