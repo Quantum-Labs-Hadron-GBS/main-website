f4a22c710ded952132dc650d73aae844609b92ec
Jayanth Ramakrishnan <jayanthvasudeva@gmail.com>
2026-07-27 19:59:15 +0530

    fix: import styles in PlanetHorizon

--- src/app/components/PlanetHorizon/PlanetHorizon.tsx
+++ src/app/components/PlanetHorizon/PlanetHorizon.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useRef } from "react";
 import * as d3 from "d3";
+import styles from "./PlanetHorizon.module.css";
 
 // Major world cities as [lng, lat] for arc line endpoints
 const CITIES: [number, number][] = [
